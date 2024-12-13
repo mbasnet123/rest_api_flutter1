5a033c9b5294b657628d65718d317c4d32de5f31
mbasnet123 <mbasnet868@gmail.com>
2024-12-13 12:26:51 +0545

    finished get request

--- lib/screens/employee_screen.dart
+++ lib/screens/employee_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:rest_api_flutter1/services/employee_services.dart';
 
+import '../models/employee.dart';
+
 class EmployeeScreen extends StatelessWidget {
   const EmployeeScreen({super.key});
 
@@ -17,12 +19,39 @@ class EmployeeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          ElevatedButton(onPressed: (){
-            // _fetchEmployeeData();
-            // EmployeeServices().getAllEmployeeData();
-            print(EmployeeServices().getAllEmployeeData(),);
-          }, child: const Text("Click to get employee data"),),
-          const Text("")
+          // ElevatedButton(onPressed: (){
+          //   // _fetchEmployeeData();
+          //   // EmployeeServices().getAllEmployeeData();
+          //   print(EmployeeServices().getAllEmployeeData(),);
+          // }, child: const Text("Click to get employee data"),),
+          Expanded(
+            child: FutureBuilder(
+              future: EmployeeServices().getAllEmployeeData(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("Couldn't fetch data. Please try again!"),
+                  );
+                }
+                if (snapshot.hasData) {
+                  var data = snapshot.data as List<Employee>;
+                  return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: CircleAvatar(backgroundImage: NetworkImage(data[index].avatar!),),
+                        title: Text("${data[index].first_name!} ${data[index].last_name!}"),
+                        subtitle: Text("${data[index].email!}"),
+                      );
+                    },
+                  );
+                }
+                else{
+                  return Center(child: CircularProgressIndicator(),);
+                }
+              },
+            ),
+          )
         ],
       ),
     );
