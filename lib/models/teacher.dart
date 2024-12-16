75fc1ec374574232477d3f59ccf39c15a850713f
mbasnet123 <mbasnet868@gmail.com>
2024-12-16 22:18:29 +0545

    making models for teacher

---
+++ lib/models/teacher.dart
@@ -0,0 +1,17 @@
+class Teacher {
+  int id;
+  String industry;
+  String location;
+  String phoneNumber;
+  String teacherName;
+
+  Teacher(
+      {required this.id,
+      required this.industry,
+      required this.location,
+      required this.phoneNumber,
+      required this.teacherName});
+}
+
+
+// api = https://retoolapi.dev/3PX2qw/data
